b7d8a1eb5f42bc0b38c2ad99d37c84c6b3c4f906
EreminNikita <eremin.learn@gmail.com>
2024-11-11 22:40:42 +0400

    add binary search for integer square root

--- leetcode.js
+++ leetcode.js
@@ -17,3 +17,23 @@
     console.log(left);
 }
 console.log(binarySearch([1,2,4,5], 6)); */
+
+// ! 69. Sqrt(x)
+/* function mySqrt(x) {
+    let left = 0;
+    let right = x;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+
+        if (mid * mid === x) {
+            return mid;
+        } else if (mid * mid < x) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return right;
+}
+console.log(mySqrt(25)); */
